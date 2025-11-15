cbaa8ed27dac23a38af689d9de692f0ab8b13e6d
seyamasan <kst_apr_24@icloud.com>
2025-11-15 16:39:00 +0900

    feat: 書き込み時のボトムシートを実装

--- app/src/main/java/com/example/nfctagwriter/NfcTagWriterScreen.kt
+++ app/src/main/java/com/example/nfctagwriter/NfcTagWriterScreen.kt
@@ -11,29 +11,46 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Nfc
+import androidx.compose.material.icons.filled.PhoneAndroid
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.Lifecycle
 import com.example.nfctagwriter.ui.theme.NFCTagWriterTheme
 import androidx.core.net.toUri
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NfcTagWriterScreen(modifier: Modifier = Modifier) {
 
     var inputText by rememberSaveable { mutableStateOf("") }
     var isWritingUrlToTag by rememberSaveable { mutableStateOf(false) }
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
 
     val context = LocalContext.current
     val activity = (context as ComponentActivity)
@@ -117,7 +134,11 @@ fun NfcTagWriterScreen(modifier: Modifier = Modifier) {
             }
         } finally {
             // 書き込み処理終了
-            isWritingUrlToTag = false
+            scope.launch { sheetState.hide() }.invokeOnCompletion {
+                if (!sheetState.isVisible) {
+                    isWritingUrlToTag = false
+                }
+            }
         }
     }
 
@@ -155,6 +176,42 @@ fun NfcTagWriterScreen(modifier: Modifier = Modifier) {
         }
     }
 
+    if (isWritingUrlToTag) {
+        // 書き込み時のボトムシート
+        ModalBottomSheet(
+            onDismissRequest = { isWritingUrlToTag = false },
+            sheetState = sheetState
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(32.dp, Alignment.CenterVertically),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Nfc,
+                    modifier = Modifier.size(48.dp),
+                    contentDescription = "Nfc icon"
+                )
+
+                Text("↑", fontWeight = FontWeight.Bold)
+
+                Icon(
+                    imageVector = Icons.Default.PhoneAndroid,
+                    modifier = Modifier.size(48.dp),
+                    contentDescription = "PhoneAndroid icon"
+                )
+
+                Text(
+                    "スマホをかざす！",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 24.sp,
+                )
+            }
+        }
+    }
+
     Box(
         modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
@@ -175,8 +232,8 @@ fun NfcTagWriterScreen(modifier: Modifier = Modifier) {
                 placeholder = { Text(text = "URLを入力")}
             )
 
-            Button(onClick = { isWritingUrlToTag = !isWritingUrlToTag }) {
-                Text(if (isWritingUrlToTag) "書き込み中..." else "書き込む")
+            Button(onClick = { isWritingUrlToTag = true }) {
+                Text("書き込む")
             }
         }
     }
